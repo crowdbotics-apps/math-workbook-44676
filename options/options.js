bba481d8f5cd1d2401109d459d7987cfca7eecdf
Crowdbotics <team@crowdbotics.com>
2023-11-20 02:45:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "math-workbook-44676",
+  url: "https://math-workbook-44676.botics.co",
+  api: "https://math-workbook-44676.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
